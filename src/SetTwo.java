d32df38339fe4479bb9c44d1155e2e6d69c12cc3
adnanrh <arhusain@ucalgary.ca>
2020-04-12 20:00:56 -0600

    set 2 c9 and c10.

--- src/SetTwo.java
+++ src/SetTwo.java
@@ -1,18 +1,14 @@
-import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.Hex;
 
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class SetTwo {
 
-    public static byte[] PKCS7Padding(byte[] bytes, int blockSize) {
+    public static byte[] padPKCS7(byte[] bytes, int blockSize) {
         int mod = bytes.length % blockSize;
         int bytesToPad = (mod != 0) ? (blockSize - mod) : 0;
         byte[] paddedBytes = new byte[bytes.length + bytesToPad];
@@ -25,7 +21,15 @@ public class SetTwo {
         return paddedBytes;
     }
 
-    public static byte[] decryptAesEcbModeBytes(byte[] encryptedBytes, byte[] key) throws Exception {
+    public static byte[] unpadPKCS7(byte[] bytes, int blockSize) {
+        int padChar = (int) bytes[bytes.length - 1];
+        if (padChar <= 0 || padChar >= blockSize) {
+            return bytes;
+        }
+        return Arrays.copyOfRange(bytes, 0, bytes.length - padChar);
+    }
+
+    private static byte[] decryptAesEcbModeBytes(byte[] encryptedBytes, byte[] key) throws Exception {
         Key aesKey = new SecretKeySpec(key, "AES");
         Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
 
@@ -33,14 +37,21 @@ public class SetTwo {
         return cipher.doFinal(encryptedBytes);
     }
 
+    private static byte[] encryptAesEcbModeBytes(byte[] plainTextBytes, byte[] key) throws Exception {
+        Key aesKey = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+
+        cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+        return cipher.doFinal(plainTextBytes);
+    }
+
     public static String decryptCBC(byte[] cipherBytes, byte[] key, byte[] iv) throws Exception {
         int blockSize = key.length;
         byte[] cipherKeyDecryptBytes = decryptAesEcbModeBytes(cipherBytes, key);
         byte[] plainTextBytes = new byte[cipherBytes.length];
 
-        byte[] firstPtBlock = Arrays.copyOfRange(cipherKeyDecryptBytes, 0, blockSize);
-        firstPtBlock = Arrays.copyOfRange(firstPtBlock, 0, blockSize);
-        firstPtBlock = SetOne.getXorBytes(firstPtBlock, iv);
+        byte[] firstAesDecryptBlock = Arrays.copyOfRange(cipherKeyDecryptBytes, 0, blockSize);
+        byte[] firstPtBlock = SetOne.getXorBytes(firstAesDecryptBlock, iv);
         for (int i = 0; i < blockSize; i++) {
             plainTextBytes[i] = firstPtBlock[i];
         }
@@ -54,6 +65,31 @@ public class SetTwo {
             }
         }
 
-        return new String(plainTextBytes, StandardCharsets.US_ASCII);
+        return new String(unpadPKCS7(plainTextBytes, blockSize), StandardCharsets.US_ASCII);
+    }
+
+    public static String encryptCBC(byte[] plainTextBytes, byte[] key, byte[] iv) throws Exception {
+        int blockSize = key.length;
+        byte[] paddedPtBytes = padPKCS7(plainTextBytes, blockSize);
+        byte[] cipherTextBytes = new byte[paddedPtBytes.length];
+
+        byte[] firstPtBlock = Arrays.copyOfRange(paddedPtBytes, 0, blockSize);
+        byte[] firstXorBlock = SetOne.getXorBytes(firstPtBlock, iv);
+        byte[] firstCtBlock = encryptAesEcbModeBytes(firstXorBlock, key);
+        for (int i = 0; i < blockSize; i++) {
+            cipherTextBytes[i] = firstCtBlock[i];
+        }
+
+        for (int i = blockSize; i < paddedPtBytes.length; i += blockSize) {
+            byte[] currentPtBlock = Arrays.copyOfRange(paddedPtBytes, i, i + blockSize);
+            byte[] previousCtBlock = Arrays.copyOfRange(cipherTextBytes, i - blockSize, i);
+            byte[] xorBlock = SetOne.getXorBytes(currentPtBlock, previousCtBlock);
+            byte[] currentCtBlock = encryptAesEcbModeBytes(xorBlock, key);
+            for (int j = 0; j < blockSize; j++) {
+                cipherTextBytes[i + j] = currentCtBlock[j];
+            }
+        }
+
+        return Base64.encodeBase64String(cipherTextBytes);
     }
 }
--- test/SetTwoTest.java
+++ test/SetTwoTest.java
@@ -2,33 +2,56 @@ import org.junit.Test;
 
 import org.apache.commons.codec.binary.Base64;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class SetTwoTest {
 
+    private static String convertCrlfsToLfs(String crlfString) {
+        return crlfString.replaceAll("\\r\\n?", "\n");
+    }
+
+    private static boolean isCrlfSystem() {
+        String s = System.lineSeparator();
+        return s.equals("\r\n");
+    }
+
     @Test
     public void challenge_nine() {
         byte[] key1 = "YELLOW SUBMARINE".getBytes(StandardCharsets.US_ASCII);
         byte[] key2 = "YELLOW SUBMARIN".getBytes(StandardCharsets.US_ASCII);
-        byte[] response = SetTwo.PKCS7Padding(key1, 20);
+        byte[] response = SetTwo.padPKCS7(key1, 20);
         assertEquals("YELLOW SUBMARINE\4\4\4\4", new String(response, StandardCharsets.US_ASCII));
-        byte[] response2 = SetTwo.PKCS7Padding(key2, 20);
+        byte[] response2 = SetTwo.padPKCS7(key2, 20);
         assertEquals("YELLOW SUBMARIN\5\5\5\5\5", new String(response2, StandardCharsets.US_ASCII));
     }
 
     @Test
-    public void challenge_ten() throws Exception {
-        String encryptedBase64 = new String(Files.readAllBytes(Paths.get("files/10.txt"))).replaceAll("[\n\r]", "");;
+    public void challenge_ten_encrypt() throws Exception {
+        String plainText = new String(Files.readAllBytes(Paths.get("files/10_pt.txt")));
+        plainText = isCrlfSystem() ? convertCrlfsToLfs(plainText) : plainText;
+        byte[] plainTextBytes = plainText.getBytes(StandardCharsets.US_ASCII);
+        byte[] key = "YELLOW SUBMARINE".getBytes(StandardCharsets.US_ASCII);
+        byte[] iv = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0".getBytes(StandardCharsets.US_ASCII);
+        String response = SetTwo.encryptCBC(plainTextBytes, key, iv);
+        String expected = new String(Files.readAllBytes(Paths.get("files/10.txt"))).replaceAll("[\n\r]", "");
+        System.out.println(response);
+        System.out.println("The system new line char(s) is: " + System.lineSeparator());
+        assertEquals(expected, response);
+    }
+
+    @Test
+    public void challenge_ten_decrypt() throws Exception {
+        String encryptedBase64 = new String(Files.readAllBytes(Paths.get("files/10.txt")));
         byte[] encryptedBytes = Base64.decodeBase64(encryptedBase64);
         byte[] key = "YELLOW SUBMARINE".getBytes(StandardCharsets.UTF_8);
         byte[] iv = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0".getBytes(StandardCharsets.US_ASCII);
         String response = SetTwo.decryptCBC(encryptedBytes, key, iv);
-        System.out.println(response);
+        String expected = new String(Files.readAllBytes(Paths.get("files/10_pt.txt")));
+        expected = isCrlfSystem() ? convertCrlfsToLfs(expected) : expected;
+        assertEquals(expected, response);
     }
 }
